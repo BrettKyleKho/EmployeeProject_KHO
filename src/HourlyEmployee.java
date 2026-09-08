92507a6563eabdac59c05ee0f9374eca1918155e
EmployeeProject_KHO <brettkkho@gmail.com>
2026-09-08 11:45:52 +0800

    Initialize Project

---
+++ src/HourlyEmployee.java
@@ -0,0 +1,17 @@
+public class HourlyEmployee {
+    private int empID;
+    private String empName;
+    private float totalHoursWorked;
+    private double ratePerHour;
+
+    public HourlyEmployee() {
+        this.empID = 0;
+        this.empName = "N/A";
+        this.totalHoursWorked = 0;
+        this.ratePerHour = 0
+    }
+
+    public HourlyEmployee(int empID, String empName) {
+
+    }
+}
