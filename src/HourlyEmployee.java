37160dafb291296d2a03e416119943975fac0ad2
EmployeeProject_KHO <brettkkho@gmail.com>
2026-09-08 11:55:38 +0800

    Update

--- src/HourlyEmployee.java
+++ src/HourlyEmployee.java
@@ -8,10 +8,65 @@ public class HourlyEmployee {
         this.empID = 0;
         this.empName = "N/A";
         this.totalHoursWorked = 0;
-        this.ratePerHour = 0
+        this.ratePerHour = 0;
     }
 
     public HourlyEmployee(int empID, String empName) {
+        this.empID = empID;
+        this.empName = empName;
+        this.totalHoursWorked = 0;
+        this.ratePerHour = 0;
+    }
 
+    public HourlyEmployee(int empID, String empName, float totalHoursWorked, double ratePerHour) {
+        this.empID = empID;
+        this.empName = empName;
+        this.totalHoursWorked = totalHoursWorked;
+        this.ratePerHour = ratePerHour;
+    }
+
+    public int getEmpID() {
+        return empID;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public float getTotalHoursWorked() {
+        return totalHoursWorked;
+    }
+
+    public double getRatePerHour() {
+        return ratePerHour;
+    }
+
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    public void setRatePerHour(double ratePerHour) {
+        this.ratePerHour = ratePerHour;
+    }
+
+    public void setTotalHoursWorked(float totalHoursWorked) {
+        this.totalHoursWorked = totalHoursWorked;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public double computeSalary(){
+
+    }
+
+    public void displayHourlyEmployee(){
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
